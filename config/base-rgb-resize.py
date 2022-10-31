48c12d9863e8f32ea4c2cdcda51b534276e7decc
Alice1820 <zhang_xifan@hotmail.com>
2022-10-31 16:08:50 +0800

    update config

--- config/base-rgb-resize.py
+++ config/base-rgb-resize.py
@@ -10,17 +10,22 @@ C = edict()
 config = C
 cfg = C
 
-C.seed = 12345
+C.seed = 15
 
-C.task = 'base-d'
+C.task = 'base-rgb-resize'
 
 remoteip = os.popen('pwd').read()
 C.root_dir = os.path.abspath(os.path.join(os.getcwd(), './'))
-C.data_dir = '/data0/xfzhang/data/NYUv2'
+C.data_dir = '/mnt/hdd/xifan/data/nyuv2-python-toolkit/NYUv2'
+# C.data_dir = '/data0/xfzhang/data/NYUv2'
+C.save_path = os.path.join(C.data_dir, 'results', C.task)
 C.abs_dir = osp.realpath(".")
 
 """Semi Learn"""
-C.modals = 'Depth'
+C.modals = 'RGB'
+C.semi = False
+C.num_labeled = None
+C.algo = 'supervised'
 
 # Dataset config
 """Dataset Path"""
@@ -41,7 +46,8 @@ C.train_source = "train"
 # C.eval_source = osp.join(C.dataset_path, "test.txt")
 C.eval_source = "test"
 C.is_test = False
-C.num_train_imgs = 795
+# C.num_train_imgs = 795
+C.num_train_imgs = C.num_labeled
 C.num_eval_imgs = 654
 C.num_classes = 40
 C.class_names =  ['wall','floor','cabinet','bed','chair','sofa','table','door','window','bookshelf','picture','counter','blinds',
@@ -51,14 +57,16 @@ C.class_names =  ['wall','floor','cabinet','bed','chair','sofa','table','door','
 
 """Image Config"""
 C.background = 255
-C.image_height = 480
-C.image_width = 640
+# C.image_height = 480
+C.image_height = 240
+# C.image_width = 640
+C.image_width = 320
 C.norm_mean = np.array([0.485, 0.456, 0.406])
 C.norm_std = np.array([0.229, 0.224, 0.225])
 
 """ Settings for network, this would be different for each kind of model"""
-C.backbone = 'mit_b2' # Remember change the path below.
-C.pretrained_model = C.root_dir + '/pretrained/segformer/mit_b2.pth'
+C.backbone = 'mit_b0' # Remember change the path below.
+C.pretrained_model = C.root_dir + '/pretrained/segformer/mit_b0.pth'
 C.decoder = 'MLPDecoder'
 C.decoder_embed_dim = 512
 C.optimizer = 'AdamW'
@@ -68,11 +76,12 @@ C.lr = 6e-5
 C.lr_power = 0.9
 C.momentum = 0.9
 C.weight_decay = 0.01
-C.batch_size = 4
+C.batch_size = 8
 C.nepochs = 2000
-C.niters_per_epoch = C.num_train_imgs // C.batch_size  + 1
+C.niters_per_epoch = C.num_train_imgs // C.batch_size
 C.num_workers = 16
-C.train_scale_array = [0.5, 0.75, 1, 1.25, 1.5, 1.75]
+# C.train_scale_array = [0.5, 0.75, 1, 1.25, 1.5, 1.75]
+C.train_scale_array = [0.5, 1.75]
 C.warm_up_epoch = 10
 
 C.fix_bias = True
@@ -84,10 +93,11 @@ C.eval_iter = 5
 C.eval_stride_rate = 2 / 3
 C.eval_scale_array = [1] # [0.75, 1, 1.25] # 
 C.eval_flip = False # True # 
-C.eval_crop_size = [480, 640] # [height weight]
+# C.eval_crop_size = [480, 640] # [height weight]
+C.eval_crop_size = [240, 320] # [height weight]
 
 """Store Config"""
-C.checkpoint_start_epoch = 150
+C.checkpoint_start_epoch = 25
 C.checkpoint_step = 25
 
 """Path Config"""
